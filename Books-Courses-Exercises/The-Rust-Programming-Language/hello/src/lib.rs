fdf5b69a4bd43c5e579bd18a82ccb81bc8f02c2f
Daniel-Sinkin <Daniel.Sinkin@gmx.de>
2023-05-16 17:33:11 +0200

    Finished the Final example in the Rust book; the last thing I should do is work through 15-17 thoroughly again and take good notes, those seem to be the most chalenging things for me right now

--- Books-Courses-Exercises/The-Rust-Programming-Language/hello/src/lib.rs
+++ Books-Courses-Exercises/The-Rust-Programming-Language/hello/src/lib.rs
@@ -1,11 +1,16 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread::{self, Thread},
+};
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
 impl ThreadPool {
-    // --snip--
     /// Create a new ThreadPool.
     ///
     /// The size is the number of threads in the pool.
@@ -16,18 +21,72 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
 
-        for _ in 0..size {
-            // create some threads and store them in the vector
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { threads }
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            println!("Shutting down Worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv();
+
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing");
+
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down");
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
