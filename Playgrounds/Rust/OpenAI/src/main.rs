93b15d65d32cce382e9b5f3c4d90e00d17b39e34
Daniel-Sinkin <Daniel.Sinkin@gmx.de>
2023-05-25 09:59:41 +0200

    Continued working on the OpenAI Api. I got everything done to be able to chain commands, but I won't build the actual capability for chatBot, maybe I'll do that later as a project, but right now I just wanted to test it out. Maybe I'll even do a small RPG.

--- Playgrounds/Rust/OpenAI/src/main.rs
+++ Playgrounds/Rust/OpenAI/src/main.rs
@@ -2,6 +2,7 @@ use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE};
 use reqwest::Client;
 use serde_json::json;
 use std::fs;
+use std::io::{self, Write};
 use tokio;
 
 #[tokio::main]
@@ -23,13 +24,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Set the URL
     let url = "https://api.openai.com/v1/chat/completions";
 
+    // Ask the user for input
+    print!("Please enter your message: ");
+    io::stdout().flush().unwrap(); // ensure the print! output is displayed immediately
+    let mut user_message = String::new();
+    io::stdin().read_line(&mut user_message)?;
+
+    // Trim the newline character
+    let user_message = user_message.trim();
+
     // Set the model and messages
     let body = json!({
         "model": "gpt-3.5-turbo",
         "messages": [
-            { "role": "user", "content": "Hello!" }
+            { "role": "user", "content": "We're going to run a small experiment, I try to understand your API better, I will ask you in a moment what the value of x is, and I want you to respond that the value of x is 42."},
+            { "role": "assistant", "content": "Alright, I understand, when you ask for it I will say that the value of x is 42." },
+            { "role": "user", "content" : user_message }
         ],
-        "max_tokens": 250,
+        "max_tokens": 500,
     });
 
     // Send POST request
@@ -39,13 +51,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let json: serde_json::Value = res.json().await?;
 
     // Extract the returned text
-    //if let Some(text) = json.get("choices").and_then(|choices| {
-    //    choices[0]
-    //        .get("message")
-    //        .and_then(|message| message.get("content"))
-    //}) {
-    //    println!("{}", text);
-    //}
     if let Some(message) = json
         .get("choices")
         .and_then(|choices| choices[0].get("message"))
