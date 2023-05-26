36bff57fda574cc55523600cfed4ccfcb6c66e67
Daniel-Sinkin <Daniel.Sinkin@gmx.de>
2023-05-26 09:29:52 +0200

    Finished the Smart pointer chapter in the rustlang book, so now only 16. Fearless Concurrency is missing. Also added the Rustinomicon book, might check that out as well, it deals with unsafe Rust code afaik, but it's being actively worked on so the current version I downloaded is not the definitive version

--- Books-Courses-Exercises/The-Rust-Programming-Language/smart_pointers/src/main.rs
+++ Books-Courses-Exercises/The-Rust-Programming-Language/smart_pointers/src/main.rs
@@ -1,20 +1,24 @@
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
 use crate::List::{Cons, Nil};
+use std::cell::RefCell;
 use std::rc::Rc;
 
 fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
+    let value = Rc::new(RefCell::new(5));
 
-    let b = Rc::new(Cons(3, Rc::clone(&a)));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
