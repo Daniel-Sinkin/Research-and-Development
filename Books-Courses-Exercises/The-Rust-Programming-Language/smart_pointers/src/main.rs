cbfa94b2a2441ffe0c063d24cafefb74731e7085
Daniel-Sinkin <Daniel.Sinkin@gmx.de>
2023-05-26 04:24:35 +0200

    Continued learning about smart pointers, should finish that and the fearless concurrency chapter today

--- Books-Courses-Exercises/The-Rust-Programming-Language/smart_pointers/src/main.rs
+++ Books-Courses-Exercises/The-Rust-Programming-Language/smart_pointers/src/main.rs
@@ -1,32 +1,20 @@
-use std::ops::Deref;
-
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
 
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-    
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-fn hello(name: &str) {
-    println!("Hello, {name}!");
-}
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
 fn main() {
-    let x = 5;
-    let y = MyBox::new(x);
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
 
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
-
-    let m = MyBox::new(String::from("Rust"));
-    hello(&m);
+    let b = Rc::new(Cons(3, Rc::clone(&a)));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
