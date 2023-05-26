2fff1c737a9e1888e6b2ef14ff4081ba2deb0163
Daniel-Sinkin <Daniel.Sinkin@gmx.de>
2023-05-26 17:53:47 +0200

    Continued Working on the linked lists, creating from vector (recursively) works, working on adding insertion

--- Playgrounds/Rust/linkedlist/src/main.rs
+++ Playgrounds/Rust/linkedlist/src/main.rs
@@ -13,11 +13,24 @@ impl ListNode {
         }
     }
 
-    fn new_vector(vec: Vec<i32>) -> Option<Self> {
-        Some(ListNode {
-            val: vec[0],
-            next: None,
-        })
+    fn from_list(vec: &[i32]) -> Option<Self> {
+        if vec.is_empty() {
+            return None;
+        }
+
+        if let Some(x) = vec.first() {
+            let mut ln = ListNode::new(*x);
+
+            let nxt = ListNode::from_list(&vec[1..]);
+
+            if let Some(y) = nxt {
+                ln.next = Some(Box::new(y));
+            }
+
+            return Some(ln);
+        } else {
+            return None;
+        }
     }
 
     fn to_string(&self) -> String {
@@ -28,6 +41,14 @@ impl ListNode {
         }
         s
     }
+
+    fn insert_after(&mut self, val: i32) {
+        if let Some(x) = &self.next {
+            panic!();
+        } else {
+            self.next = Some(Box::new(ListNode::new(val)));
+        }
+    }
 }
 
 fn main() {
@@ -35,7 +56,28 @@ fn main() {
     let mut ln_next: ListNode = ListNode::new(1);
     let ln_next_next: ListNode = ListNode::new(2);
     ln_next.next = Some(Box::new(ln_next_next));
+
     ln.next = Some(Box::new(ln_next));
 
-    println!("{}", ln.to_string());
+    let v: Vec<i32> = vec![1, 2, -7, 4];
+
+    dbg!(&v);
+    dbg!(&v[1..]);
+    dbg!(&v[2..]);
+    dbg!(&v[3..]);
+    dbg!(&v[4..]);
+
+    let x = ListNode::from_list(&v);
+
+    let mut y = ListNode::from_list(&v[3..]).unwrap();
+
+    if let Some(y) = x {
+        println!("{}", y.to_string());
+    } else {
+        println!("Empty!");
+    }
+
+    y.insert_after(-10);
+
+    println!("{}", y.to_string());
 }
