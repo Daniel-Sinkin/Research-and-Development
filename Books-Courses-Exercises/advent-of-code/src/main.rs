53d1f7b6f463706c38079900173ad551801ba865
Daniel-Sinkin <Daniel.Sinkin@gmx.de>
2023-05-31 23:41:04 +0200

    Continued working on the advent of code problems, almost got day 8 working, but my solution is terrible, might be the worst code I've written yet

--- Books-Courses-Exercises/advent-of-code/src/main.rs
+++ Books-Courses-Exercises/advent-of-code/src/main.rs
@@ -1,19 +1,16 @@
 use std::fs::File;
 use std::io::{self, Read};
-use std::vec;
 
 const FILENAME: &str = "day8";
-#[allow(unused_mut)]
-#[allow(unused_variables)]
+const ENABLE_OUTPUT: bool = false;
 fn main() -> io::Result<()> {
-    let file = File::open(FILENAME);
+    let _file = File::open(FILENAME);
 
     if let Ok(mut file) = File::open(FILENAME) {
         let mut body = String::new();
         file.read_to_string(&mut body)?;
 
         let mut grid: Vec<Vec<u8>> = Default::default();
-        let mut grid: Vec<Vec<bool> = Default::default();
 
         for line in body.lines() {
             let mut row: Vec<u8> = Default::default();
@@ -24,12 +21,72 @@ fn main() -> io::Result<()> {
             grid.push(row);
         }
 
+        let mut count = 0;
         for y in 1..=98 {
             for x in 1..=98 {
-                for 
+                let mut hidden = false;
+                let curr = &grid[y][x];
+                for h in 0..y {
+                    if &grid[h][x] >= curr {
+                        hidden = true;
+                        break;
+                    }
+                }
+
+                if !hidden {
+                    if ENABLE_OUTPUT {
+                        println!("\tCell at <y = {}, x = {}> is not hidden (1)", y, x);
+                    }
+                } else {
+                    for h in (y + 1)..99 {
+                        if &grid[h][x] >= curr {
+                            hidden = true;
+                            break;
+                        }
+                    }
+
+                    if !hidden {
+                        if ENABLE_OUTPUT {
+                            println!("\tCell at <y = {}, x = {}> is not hidden (1)", y, x);
+                        }
+                    } else {
+                        for v in 0..x {
+                            if &grid[y][v] >= curr {
+                                hidden = true;
+                                break;
+                            }
+                        }
+
+                        if !hidden {
+                            if ENABLE_OUTPUT {
+                                println!("\tCell at <y = {}, x = {}> is not hidden (1)", y, x);
+                            }
+                        } else {
+                            for v in (x + 1)..99 {
+                                if &grid[y][v] >= curr {
+                                    hidden = true;
+                                    break;
+                                }
+                            }
+
+                            if !hidden {
+                                if ENABLE_OUTPUT {
+                                    println!("\tCell at <y = {}, x = {}> is not hidden (1)", y, x);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if hidden {
+                    if ENABLE_OUTPUT {
+                        println!("\tCell at <y = {}, x = {}> is hidden!", y, x);
+                    }
+                    count += 1;
+                }
             }
-            println!("{:?}", &grid[y]);
         }
+        dbg!(count);
     } else {
         return Err(io::Error::new(
             io::ErrorKind::Other,
